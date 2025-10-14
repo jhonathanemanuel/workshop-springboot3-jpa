3fdfce098f109531c93b0b0854ea7bb8543c14d2
jhonathanemanuel <jhonathanemanuelgg@gmail.com>
2025-10-14 15:04:36 -0300

    Project created

---
+++ src/main/java/com/minhaempresa/course_077/Course077Application.java
@@ -0,0 +1,13 @@
+package com.minhaempresa.course_077;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Course077Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Course077Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/minhaempresa/course_077/Course077ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minhaempresa.course_077;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Course077ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
