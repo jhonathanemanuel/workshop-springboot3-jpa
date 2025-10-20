40aa911471fad9999159a00acaaef1fb102b5a6a
jhonathanemanuel <jhonathanemanuelgg@gmail.com>
2025-10-20 14:16:55 -0300

    Exception handling - findById

---
+++ src/main/java/com/minhaempresa/course_077/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,9 @@
+package com.minhaempresa.course_077.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+	
+	public ResourceNotFoundException(Object id) {
+		super("Resource not found. Id " + id);
+	}
+}
